1a55ba169007636fe1c8c0373403c83e8040f452
Bobbi-Jo Clark <hermes98761234@tutamail.com>
2026-06-27 15:26:38 +0000

    fmt: fix formatting in lib.rs, add .gitignore

--- crates/ervy/src/lib.rs
+++ crates/ervy/src/lib.rs
@@ -9,12 +9,14 @@ pub mod gauge;
 pub mod pie;
 pub mod scatter;
 
-pub use color::{bg, fg, Color, Style};
-pub use datum::{Datum, DatumValue};
-pub use options::{BarOptions, BulletOptions, DonutOptions, GaugeOptions, PieOptions, ScatterOptions};
 pub use bar::bar;
 pub use bullet::bullet;
+pub use color::{bg, fg, Color, Style};
+pub use datum::{Datum, DatumValue};
 pub use donut::donut;
 pub use gauge::gauge;
+pub use options::{
+    BarOptions, BulletOptions, DonutOptions, GaugeOptions, PieOptions, ScatterOptions,
+};
 pub use pie::pie;
 pub use scatter::scatter;
